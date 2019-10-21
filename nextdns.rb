a61cb0027b36d7142c56272b9dbaa231a23a56a2
NextDNS Team <team@nextdns.io>
2019-10-21 01:44:10 -0700

    Brew formula update for nextdns version v1.0.3

--- nextdns.rb
+++ nextdns.rb
@@ -2,16 +2,16 @@
 class Nextdns < Formula
   desc "NextDNS DNS/53 to DoH Proxy"
   homepage "https://nextdns.io"
-  version "1.0.2"
+  version "1.0.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/nextdns/nextdns/releases/download/v1.0.2/nextdns_1.0.2_darwin_amd64.tar.gz"
-    sha256 "cb12af88942479a643edda2b96a16729835edd30981ccfcf9773efd336d75461"
+    url "https://github.com/nextdns/nextdns/releases/download/v1.0.3/nextdns_1.0.3_darwin_amd64.tar.gz"
+    sha256 "080afa0a7689574fc7eb4fd510ce2624fe6d69737a09f1e10ffe1a406180b5fd"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/nextdns/nextdns/releases/download/v1.0.2/nextdns_1.0.2_linux_amd64.tar.gz"
-      sha256 "7a62c7e57a0b8fe5886d4f0b50efaf0a7ccf00141511e97bf6b8efa330bff895"
+      url "https://github.com/nextdns/nextdns/releases/download/v1.0.3/nextdns_1.0.3_linux_amd64.tar.gz"
+      sha256 "007991eed874a18f1a90e8cbebbe6e872daa5b62761e1ceb5d100d20a131ada0"
     end
   end
 
