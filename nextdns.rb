a5c18c4bbaea745bff33330882da1f78726a075f
NextDNS Team <team@nextdns.io>
2020-03-30 11:22:46 -0700

    Brew formula update for nextdns version v1.4.36

--- nextdns.rb
+++ nextdns.rb
@@ -2,24 +2,24 @@
 class Nextdns < Formula
   desc "NextDNS DNS/53 to DoH Proxy"
   homepage "https://nextdns.io"
-  version "1.4.35"
+  version "1.4.36"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/nextdns/nextdns/releases/download/v1.4.35/nextdns_1.4.35_darwin_amd64.tar.gz"
-    sha256 "a4e5cc2dc19bd6a54be0cfe4b93129d6b2ea4518803680e19fb3445c37bf1517"
+    url "https://github.com/nextdns/nextdns/releases/download/v1.4.36/nextdns_1.4.36_darwin_amd64.tar.gz"
+    sha256 "3d6dc9e0a8515d9193a503b6a5643b1ba92b61c62d2c40a033c31dadefed5ea8"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/nextdns/nextdns/releases/download/v1.4.35/nextdns_1.4.35_linux_amd64.tar.gz"
-      sha256 "b6bdfe5d5577b6abab985de5748c23e0b27d50b500b89671e09b8235f3bd6ff4"
+      url "https://github.com/nextdns/nextdns/releases/download/v1.4.36/nextdns_1.4.36_linux_amd64.tar.gz"
+      sha256 "2496f79fda4c5e002a2503942e5a4a988f1767091e284f8d6f4f4cb905cfaa1e"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/nextdns/nextdns/releases/download/v1.4.35/nextdns_1.4.35_linux_arm64.tar.gz"
-        sha256 "93b58b64a123ad7f35f771e99c311755910914cb1731920d711991251436f964"
+        url "https://github.com/nextdns/nextdns/releases/download/v1.4.36/nextdns_1.4.36_linux_arm64.tar.gz"
+        sha256 "3d94acacc432340cc81ad4467a19f2d9008adab50c637a29b3eea35528774135"
       else
-        url "https://github.com/nextdns/nextdns/releases/download/v1.4.35/nextdns_1.4.35_linux_armv6.tar.gz"
-        sha256 "2f8df3b718d705d7f1846d3686ce90895ac6a1816b8a028ee4965fb07e512552"
+        url "https://github.com/nextdns/nextdns/releases/download/v1.4.36/nextdns_1.4.36_linux_armv6.tar.gz"
+        sha256 "e54cef37a379ca44c4e1d16b4efcafdf85bf54782640456ee8ef0083e87e1b2f"
       end
     end
   end
